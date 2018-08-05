ccd0812975e9516cdc57da8636479971d1f5d561
melkshake <melkshake@kilograpp.com>
2018-08-05 21:50:44 +0400

    Initial commit for FileUtil class.

---
+++ src/com/melkonyan/readfromfile/FileUtil.java
@@ -0,0 +1,15 @@
+package com.melkonyan.readfromfile;
+
+import java.io.File;
+
+public final class FileUtil implements FileUtilMethods {
+  @Override
+  public String readDataFromFile(File file) {
+    return null;
+  }
+
+  @Override
+  public boolean writeDataIntoFile() {
+    return false;
+  }
+}
---
+++ src/com/melkonyan/readfromfile/FileUtilMethods.java
@@ -0,0 +1,8 @@
+package com.melkonyan.readfromfile;
+
+import java.io.File;
+
+public interface FileUtilMethods {
+  String readDataFromFile(File file);
+  boolean writeDataIntoFile();
+}
